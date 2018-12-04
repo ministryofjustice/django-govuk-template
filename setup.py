4e0eca75af603e4dfd5e399d41ebe26dfae24373
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-12-04 15:23:12 +0000

    Indicate that python 3.7 is supported

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     cmdclass=setup_extensions.command_classes,
     setup_requires=setup_requires,
